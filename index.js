fe90acdc9e29222c2449a5f386b9f9a645147b0e
morales.chris1989@gmail.com <morales.chris1989@gmail.com>
2018-07-11 22:37:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-var animal = 'dog'
 
+ var animal = 'dog'
 function myAnimal() {
+  
   return animal
+ 
 }
 
 function yourAnimal() {
@@ -9,6 +11,7 @@ function yourAnimal() {
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
   return animal
+ 
 }
 
 function add2(n) {
@@ -26,4 +29,5 @@ var funkyFunction = function() {
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
-var theFunk = funkyFunction
+return 
+
